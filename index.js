665b0ea1d2fe14891c3c4a04f9c2036724cb22b2
Jordan Thomas <jthomas135@hotmail.com>
2022-05-15 13:41:06 -0700

    added prompts for github username and email address. began work on generateMarkdown.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 
 // TODO: Create an array of questions for user input
 const questions = () => {
+    // if(!data){
+    //     data = {}
+    // }
     return inquirer.prompt([
         {
             type: 'input',
@@ -90,20 +94,57 @@ const questions = () => {
             name: 'licenses',
             message: 'Which license would you like to use for your project?',
             choices: ['MIT', 'more licenses', 'even more', 'and another']
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github username?',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your github username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter a valid email address at which you can be reached.',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter testing instructions for your project!');
+                    return false;
+                }
+            }
         }
     ])
+    // .then(data => {
+    //     return questions(data);
+    // });
 };
 
 questions()
-    .then(answers => {
-        console.log(answers);
+.then(answers => {
+    console.log(answers);
+    console.log(answers.title)
+    console.log(answers.github)
+    console.log(answers.email)
     });
 
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    // fs.writeFile(fileName, data, err => {
+    //     // conditional if error, else
+    // })
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,9 +12,37 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  1. [Installation Instructions](#installation-instructions)
+  2. [Usage Information](#usage-information)
+  3. [Contribution Guidelines](#contribution-guidelines)
+  4. [Test Instructions](#test-instructions)
+  
+  ## Installation Instructions
+  ${data.installation}
 
-`;
+  ## Usage Information
+  ${data.usage}
+
+  ## Contribution Guidelines
+  ${data.contribution}
+
+  ## Test Instructions
+  ${data.test}
+
+  ## License
+  (use the license functions to fill this space)
+
+  ## Questions
+  https://github.com/${data.github}
+  email: ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
